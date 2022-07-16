05365d3973e059911c341b3b01e2a3b2a0c591fd
danimdev <106762674+danimdev@users.noreply.github.com>
2022-07-16 12:34:51 +0200

    Create RegisterLoginManager.cs

---
+++ RegisterLoginManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace RegisterLoginApp
+{
+    public class RegisterLoginManager
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public bool EncryptToFile(string username,string password)
+        {
+            var file = @"passwordFile.txt";
+
+            FileInfo fileToDecrypt = new FileInfo(file);
+
+            try
+            {
+                using (FileStream fileToWrite = new FileStream(file, FileMode.OpenOrCreate))
+                {
+                    using (StreamWriter writeToFile = new StreamWriter(fileToWrite, Encoding.UTF8))
+                    {
+                        writeToFile.Write(password);
+                        fileToDecrypt.Encrypt();
+                        MessageBox.Show("Encrypted Successfully!");
+                        return true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+            return false;
+        }
+
+        public bool CheckEncryptedPassword(string password)
+        {
+            return false;
+        }
+
+    }
+}
